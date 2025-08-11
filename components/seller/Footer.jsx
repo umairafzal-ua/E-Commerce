f090aca2bee09c124cea97ddf2a4e6335b0eaaf7
Muhammad Umair <umaair40@gmail.com>
2025-08-12 01:41:46 +0500

    Update Footer.jsx

--- components/seller/Footer.jsx
+++ components/seller/Footer.jsx
@@ -6,10 +6,13 @@ const Footer = () => {
   return (
     <div className="flex md:flex-row flex-col-reverse items-center justify-between text-left w-full px-10">
       <div className="flex items-center gap-4">
-        <Image className="hidden md:block" src={assets.logo} alt="logo" />
+        <div className="cursor-pointer w-28 md:w-32 flex items-center justify-center md:gap-2">
+        <span className="text-orange-600 font-extrabold text-2xl md:text-3xl">E</span>
+        <span className="text-xl md:text-2xl "> Commerce</span>
+      </div>
         <div className="hidden md:block h-7 w-px bg-gray-500/60"></div>
         <p className="py-4 text-center text-xs md:text-sm text-gray-500">
-          Copyright 2025 © greatstack.dev All Right Reserved.
+          Copyright 2025 © All Right Reserved.
         </p>
       </div>
       <div className="flex items-center gap-3">
